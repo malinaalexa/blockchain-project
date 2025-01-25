895e4b76b8b93d01061658782f41441566633b59
Steica Malina-Aexa <104028370+malinaalexa@users.noreply.github.com>
2025-01-26 01:05:47 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"; // Remove this line if you don't have index.css
+import "./index.css"; 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
